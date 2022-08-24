42f6d217fcddf4cfdef0e8b029b62044a6df8de7
Trung <trunghuynh1999@gmail.com>
2022-08-24 13:32:36 +0700

    update theme support ipad

--- lib/extensions/theme_extension.dart
+++ lib/extensions/theme_extension.dart
@@ -345,4 +345,292 @@ extension TextThemeExtension on TextTheme {
       fontFamily: AppTheme.iBMPlexMono,
     );
   }
+
+  TextStyle get linkStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? Colors.transparent : Colors.transparent,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+      fontWeight: FontWeight.w400,
+      shadows: [Shadow(color: Colors.black, offset: Offset(0, -1))],
+      decoration: TextDecoration.underline,
+      decorationStyle: TextDecorationStyle.solid,
+      decorationColor: Colors.black,
+      decorationThickness: 1.1,
+    );
+  }
+
+  TextStyle get linkStyle14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? Colors.transparent : Colors.transparent,
+      fontSize: 14,
+      fontFamily: AppTheme.atlasGrotesk,
+      fontWeight: FontWeight.w700,
+      shadows: [Shadow(color: Colors.black, offset: Offset(0, -1))],
+      decoration: TextDecoration.underline,
+      decorationStyle: TextDecorationStyle.solid,
+      decorationColor: Colors.black,
+      decorationThickness: 1.1,
+    );
+  }
+
+  TextStyle get whitelinkStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? Colors.transparent : Colors.transparent,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+      shadows: [Shadow(color: Colors.white, offset: Offset(0, -1))],
+      decoration: TextDecoration.underline,
+      decorationStyle: TextDecorationStyle.solid,
+      decorationColor: Colors.white,
+      decorationThickness: 1.1,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  TextStyle get dateDividerTextStyle14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode
+          ? AppColor.chatDateDividerColor
+          : AppColor.chatDateDividerColor,
+      fontSize: 14,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get sentMessageBodyTextStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get sentMessageCaptionTextStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get receivedMessageCaptionTextStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get receivedMessageBodyTextStyle16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.white : AppColor.white,
+      fontSize: 16,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get ibmGreyMediumNormal20 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.iBMPlexMono,
+      fontSize: 20,
+      color: _isLightMode ? AppColor.greyMedium : AppColor.greyMedium,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  TextStyle get atlasBlackBold14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 14,
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  TextStyle get atlasBlackNormal16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 16,
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  TextStyle get atlasGreyNormal16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 16,
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  TextStyle get atlasSpanishGreyNormal20 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 20,
+      color: _isLightMode
+          ? AppColor.secondarySpanishGrey
+          : AppColor.secondarySpanishGrey,
+      fontWeight: FontWeight.w300,
+    );
+  }
+
+  TextStyle get atlasSpanishGreyBold20 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 20,
+      color: _isLightMode
+          ? AppColor.secondarySpanishGrey
+          : AppColor.secondarySpanishGrey,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  TextStyle get atlasWhiteItalic14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      fontFamily: AppTheme.atlasGrotesk,
+      fontSize: 14,
+      color: _isLightMode ? AppColor.white : AppColor.white,
+      fontStyle: FontStyle.italic,
+    );
+  }
+
+  TextStyle get atlasDimgreyBold16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get atlasWhiteBold16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.white : AppColor.white,
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      fontFamily: AppTheme.atlasGrotesk,
+    );
+  }
+
+  TextStyle get ibmGreyBold16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get ibmBlackNormal16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.primaryBlack : AppColor.primaryBlack,
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get atlasGreyBold14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get atlasGreyUnderline14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+      decoration: TextDecoration.underline,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get ibmWhiteNormal16 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color: _isLightMode ? AppColor.white : AppColor.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w300,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get ibmGreyNormal14 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
+
+  TextStyle get ibmGreyNormal20 {
+    final bool _isLightMode =
+        SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+    return TextStyle(
+      color:
+          _isLightMode ? AppColor.secondaryDimGrey : AppColor.secondaryDimGrey,
+      fontSize: 20,
+      fontWeight: FontWeight.w300,
+      fontFamily: AppTheme.iBMPlexMono,
+    );
+  }
 }
--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -166,6 +166,160 @@ class AppTheme {
     );
   }
 
+  static ThemeData tabletLightTheme() {
+    return ThemeData(
+      backgroundColor: AppColor.white,
+      primaryColor: AppColor.primaryBlack,
+      scaffoldBackgroundColor: AppColor.white,
+      colorScheme: ColorScheme(
+        primary: AppColor.primaryBlack,
+        onPrimary: AppColor.primaryBlack,
+        secondary: AppColor.white,
+        onSecondary: AppColor.white,
+        background: AppColor.white,
+        onBackground: AppColor.white,
+        brightness: Brightness.light,
+        error: AppColor.red,
+        onError: AppColor.red,
+        surface: AppColor.secondaryDimGrey,
+        onSurface: AppColor.secondaryDimGrey,
+      ),
+      errorColor: Colors.red,
+      primaryIconTheme:
+          const IconThemeData(color: AppColor.primaryBlack, size: 24),
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 36,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline2: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 24,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline3: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 22,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline4: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline5: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 14,
+          fontFamily: atlasGrotesk,
+        ),
+        button: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+          fontFamily: iBMPlexMono,
+        ),
+        caption: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          fontFamily: iBMPlexMono,
+        ),
+        bodyText1: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 20,
+          fontFamily: atlasGrotesk,
+          fontWeight: FontWeight.w300,
+        ),
+        bodyText2: TextStyle(
+          color: AppColor.secondaryDimGrey,
+          fontSize: 20,
+          fontFamily: iBMPlexMono,
+        ),
+        subtitle1: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 20,
+          fontFamily: iBMPlexMono,
+        ),
+        subtitle2: TextStyle(
+          color: AppColor.primaryBlack,
+          fontSize: 14,
+          fontWeight: FontWeight.w300,
+          fontFamily: iBMPlexMono,
+        ),
+      ),
+      primaryTextTheme: TextTheme(
+        headline1: TextStyle(
+          color: AppColor.white,
+          fontSize: 36,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline2: TextStyle(
+          color: AppColor.white,
+          fontSize: 24,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline3: TextStyle(
+          color: AppColor.white,
+          fontSize: 22,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline4: TextStyle(
+          color: AppColor.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          fontFamily: atlasGrotesk,
+        ),
+        headline5: TextStyle(
+          color: AppColor.white,
+          fontSize: 14,
+          fontFamily: atlasGrotesk,
+        ),
+        button: TextStyle(
+          color: AppColor.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+          fontFamily: iBMPlexMono,
+        ),
+        caption: TextStyle(
+          color: AppColor.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          fontFamily: iBMPlexMono,
+        ),
+        bodyText1: TextStyle(
+          color: AppColor.white,
+          fontSize: 20,
+          fontFamily: atlasGrotesk,
+          fontWeight: FontWeight.w300,
+        ),
+        bodyText2: TextStyle(
+          color: AppColor.white,
+          fontSize: 20,
+          fontFamily: iBMPlexMono,
+        ),
+        subtitle1: TextStyle(
+          color: AppColor.white,
+          fontSize: 20,
+          fontFamily: iBMPlexMono,
+        ),
+        subtitle2: TextStyle(
+          color: AppColor.white,
+          fontSize: 14,
+          fontWeight: FontWeight.w300,
+          fontFamily: iBMPlexMono,
+        ),
+      ),
+    );
+  }
+
   static ThemeData darkTheme() {
     return ThemeData();
   }
