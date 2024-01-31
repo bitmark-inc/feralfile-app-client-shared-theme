75b30a8f41124dd1afaaf05201e21e0268a723b5
PuPha <nguyensanghso@gmail.com>
2024-01-31 09:02:22 +0700

    refactor: update file name

--- lib/extensions/theme_extension/moma_sans.dart
+++ lib/extensions/theme_extension/moma_sans.dart
@@ -1,4 +1,4 @@
-import 'package:feralfile_app_theme/autonomy_theme.dart';
+import 'package:feralfile_app_theme/feral_file_app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
